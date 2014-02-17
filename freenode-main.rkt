ac03714a033940473d49d3fcbd0883e81c787b4e
Asumu Takikawa <asumu@ccs.neu.edu>
2014-02-17 17:03:25 -0500

    Allow nickserv password to be specified with a flag

--- freenode-main.rkt
+++ freenode-main.rkt
@@ -19,7 +19,9 @@
   (clearenv)
   (command-line
    #:program "rudybot"
-   #:once-each)
+   #:once-each
+   [("-p" "--nickserv-password") password "Nickserv password"
+                                 (*nickserv-password* password)])
 
   ;; TODO -- the equivalent of "echo -17 > /proc/self/oom_adj", to
   ;; immunize this process from being killed by the dreaded oom_killer
